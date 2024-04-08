87ec935a6f7d57548cae873aceac3b164021f9cf
Colin PROKOPOWICZ <colin.prokopowicz@etu.univ-littoral.fr>
2024-04-08 22:48:38 +0200

    changes

---
+++ astro.config.mjs
@@ -0,0 +1,18 @@
+import { defineConfig } from "astro/config";
+
+import tailwind from "@astrojs/tailwind";
+
+const owner = "astrojs";
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwind()],
+  i18n: {
+    locales: ["en", "fr"],
+    defaultLocale: "fr",
+    routing: {
+      prefixDefaultLocale: false,
+    }
+  },
+  site: 'http://Colnup.github.io',
+  base: 'portfolio-sae',
+});
